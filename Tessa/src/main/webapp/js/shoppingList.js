61788c83e463ed8863f52298dd31758eb0601943
Korti <lucas.korthauer@gmail.com>
2016-06-12 19:12:02 +0200

    Eingabe Modal hinzugefügt um einen neuen List Eintrag zuerstellen. Floating Action Button hinzugefügt um das Eingabe Modal anzuzeigen.

--- Tessa/src/main/webapp/js/shoppingList.js
+++ Tessa/src/main/webapp/js/shoppingList.js
@@ -1,6 +1,11 @@
 var baseURL = "/Tessa/rs/shoppinglist";
 
 $(function () {
+
+    $('.modal-trigger').leanModal();
+
+    $('.tooltipped').tooltip({delay: 50});
+
     $('.button-collapse').sideNav();
 });
 
@@ -32,30 +37,18 @@ function generateTable(table){
 function Send(){
     var request = {};
     var helper = 1;
-    if(!document.getElementById("anzahlEingabe").value.match("^([0-9]{1,2}|100)$")||document.getElementById("anzahlEingabe").value==0){
-        document.getElementById("errorAnzahl").style.color = "red";
-        document.getElementById("anzahlEingabe").style.borderColor = "red";
-        helper = 0;
 
-    }
-    else{
-        document.getElementById("errorAnzahl").style.color = "white";
-        document.getElementById("anzahlEingabe").style.borderColor = "lightgrey";
-    }
-    if(!document.getElementById("produktEingabe").value.match("^([A-Za-z]|[\u00c4\u00e4\u00d6\u00f6\u00dc\u00fc\u00df])+( |[A-Za-z]|[\u00c4\u00e4\u00d6\u00f6\u00dc\u00fc\u00df])*[A-Za-z]$")){
-        document.getElementById("errorProdukt").style.color = "red";
-        document.getElementById("produktEingabe").style.borderColor = "red";
+    if(!$('#productInput').val().match("^[A-Z].{1,}$")){
+        $('#productInput').addClass('invalid');
         helper=0;
     }
     else{
-        document.getElementById("errorProdukt").style.color = "white";
-        document.getElementById("produktEingabe").style.borderColor = "lightgrey";
+        $('#productInput').removeClass('invalid');
     }
 
     if(helper == 1){
-        request['barcode'] = "0000000000";
-        request['name'] = document.getElementById("produktEingabe").value;
-        request['stueck'] = document.getElementById("anzahlEingabe").value;
+        request['name'] = $('#productInput').val();
+        request['stueck'] = $('#countRange').val();
         request['permanent'] = true;
 
         //Schickt Produkt an den Application Server - RestService Abfrage
@@ -63,37 +56,23 @@ function Send(){
             type: "POST",
             url: baseURL,
             data: JSON.stringify(request),
-            contentType: "application/json"
+            contentType: "application/json",
+            success: function(eintrag) {
+                Materialize.toast('Eintrag gespeichert!', 4000);
+                //addProductEntryToList(product, $("#productList").children().length + 1);
+                clearInputFields();
+            }
+        }).fail(function(){
+            Materialize.toast('Eintrag konnte nicht gespeichert werden!', 4000);
         });
-
-       // Delete();
-    }
-    else{
-        alert("error");
     }
 }
-function Delete(){
-    document.getElementById("produktEingabe").value = "";
-    document.getElementById("anzahlEingabe").value = "";
-    document.getElementById("Tag").value = "Tag";
-    document.getElementById("Monat").value = "Monat";
-    document.getElementById("Jahr").value = "Jahr";
+
+function clearInputFields(){
+    $('#productInput').val("");
+    $('#countRange').val(1);
+    $('#expirationDate').val("");
+
+    $('#labelProduct').removeClass("active");
+    $('#productInput').removeClass("valid");
 }
-
-// Change the selector if needed
-var $table = $('table.scroll'),
-    $bodyCells = $table.find('tbody tr:first').children(),
-    colWidth;
-
-// Adjust the width of thead cells when window resizes
-$(window).resize(function() {
-    // Get the tbody columns width array
-    colWidth = $bodyCells.map(function() {
-        return $(this).width();
-    }).get();
-
-    // Set the width of thead columns
-    $table.find('thead tr').children().each(function(i, v) {
-        $(v).width(colWidth[i]);
-    });
-}).resize(); // Trigger resize handler
